c636c288fdaf10757d607aecfd9c2b54496bbed3
Archpumpkin17 <118905959+Archpumpkin17@users.noreply.github.com>
2023-05-11 22:47:27 +0100

    Update website.js

--- website.js
+++ website.js
@@ -2,5 +2,6 @@ var randomcolor = Math.floor(Math.random() * 16777215).toString(16);
 var color = '#' + randomcolor
 
 function change() {
-    document.body.style.background = color }
+    document.body.style.background = color;
+    console.log(color);
 }
